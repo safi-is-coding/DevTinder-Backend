3468f3f921c448c6ed227c4d43aa58d853109ca4
safi-is-coding <cse.mohammadsafi@gmail.com>
2025-09-11 21:34:45 +0530

    `Update db connection URL to use environment variable`

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async()=> {
     await mongoose.connect(
-        "mongodb+srv://safimaz:safimaz123@backenddb.wphyzpb.mongodb.net/DevTinder"
+        "process.env.MONGODB_URL"
     )
 }
 
