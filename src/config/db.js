c00e10e3c0e3331a4a34c28e79402cf2d00a9524
safi-is-coding <cse.mohammadsafi@gmail.com>
2025-09-11 21:31:20 +0530

    `Update cloudinary and database configurations with hardcoded values`

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async()=> {
     await mongoose.connect(
-        process.env.MONGODB_URL
+        "mongodb+srv://safimaz:safimaz123@backenddb.wphyzpb.mongodb.net/DevTinder"
     )
 }
 
