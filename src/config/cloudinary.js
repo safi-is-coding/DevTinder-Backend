2d29269f486a1ccd0d4ab27ab41e8f09eb6dfa76
safi-is-coding <cse.mohammadsafi@gmail.com>
2025-09-13 09:59:55 +0530

    `Update CORS origin, Cloudinary config, and MongoDB URL in config files`

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -3,9 +3,9 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dpfb1od3c",
+  api_key: process.env.CLOUDINARY_API_KEY || "988836346312584",
+  api_secret: process.env.CLOUDINARY_API_SECRET || "h4pbbvxXRBse9QMvcz4I8iL9KL4",
 });
 
 const storage = new CloudinaryStorage({
